398524669056482d0cd91833c87c6192d5db1684
BeRiriJ <rachelzhangyunjia@gmail.com>
2024-03-18 22:53:22 +0800

    create YOLOv9 project

--- 02_import_and_load_model.py
+++ 02_import_and_load_model.py
@@ -1,2 +1,10 @@
 from ultralytics import YOLO
 # !pip install ultralytics
+
+# Load a model
+# model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+source = '02_source_image.jpg'
+# results = model(source=source, show=True, conf=0.1, save=True, device='mps')
+results = model.predict(source=source, show=True, conf=0.1, save=True, project='runs', name='detect')
+print(results)
